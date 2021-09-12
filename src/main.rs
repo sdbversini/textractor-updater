1c6637d2568a17ebcd704440394760071036da5d
Bastien VERSINI <bastien.versini@gmail.com>
2021-09-12 23:12:19 +0200

    feat: parse argument to select architecture
    fixes #6

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 use crate::updater::Updater;
+use clap::App;
+use clap::Arg;
 
 mod updater;
 
 fn main() {
+    let clap_matches = App::new("Textractor-Updater")
+        .version("0.5")
+        .author("Bastien V")
+        .about("Updates Textractor and launches it")
+        .arg(
+            Arg::with_name("architecture")
+                .short("a")
+                .long("arch")
+                .value_name("ARCH")
+                .help(r#"Sets architecture of the executable to launch. Possibles values are "x86", "86", "x64" and "64""#)
+                .required(true),
+        ).get_matches();
+
     let mut updater = Updater::new();
-    updater.update_and_run("x86");
+    if let Some(arch) = clap_matches.value_of("architecture") {
+        match arch {
+            "x86" | "86" => updater.update_and_run("x86"),
+            "x64" | "64" => updater.update_and_run("x64"),
+            _ => {
+                eprintln!("Improper argument, falling back to x86");
+                updater.update_and_run("x86")
+            }
+        }
+    }
 }
